c43c24922fe8e27de73ebfb28e74381813d5c289
karimalrawas <kemonu1@gmail.com>
2023-05-19 14:11:47 +0000

    update django view

--- portfolio_history/history_news/views.py
+++ portfolio_history/history_news/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Article
+
+def latest_news(request):
+    articles = Article.objects.all().order_by('-publish_date')[:5]
+    return render(request, 'latest_news.html', {'articles': articles})
