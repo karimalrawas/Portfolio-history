0f2b6b9bf880276cd835f02a8fa1a9d8e989ee86
karimalrawas <kemonu1@gmail.com>
2023-05-19 14:35:38 +0000

    update view amd url files to link sign up and login pages

--- portfolio_history/history_news/views.py
+++ portfolio_history/history_news/views.py
@@ -6,3 +6,35 @@ from .models import Article
 def latest_news(request):
     articles = Article.objects.all().order_by('-publish_date')[:5]
     return render(request, 'latest_news.html', {'articles': articles})
+
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import redirect
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Replace 'home' with the URL name for your homepage
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  # Replace 'home' with the URL name for your homepage
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+
